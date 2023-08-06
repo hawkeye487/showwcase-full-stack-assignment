d8cbb6839812ad4c0a0e84b834ee8fc31945da04
Apra487 <apra487@gmail.com>
2023-08-07 03:34:34 +0530

    feat: add autocompleteion for the school field

--- frontend/src/components/EducationForm.tsx
+++ frontend/src/components/EducationForm.tsx
@@ -1,364 +1,349 @@
-// import React, { useState } from 'react';
-// // import axios from 'axios';
-// // import Autocomplete from '@material-ui/lab/Autocomplete';
-// // import TextField from '@material-ui/core/TextField';
-
-// interface EducationFormProps {
-//   onSubmit: (education: string) => void;
-//   onCancel: () => void;
-// }
-
-// const EducationForm: React.FC<EducationFormProps> = ({ onSubmit, onCancel }) => {
-//   const [school, setSchool] = useState<string>('');
-
-//   // API call to fetch school names for auto-completion
-//   // const fetchSchoolNames = async (searchValue: string) => {
-//   //   // const response = await axios.get(`API_URL?q=${searchValue}`);
-//   //   return response.data; // Assuming the API returns an array of school names
-//   // };
-
-//   const handleSubmit = () => {
-//     // Validate the data if needed
-
-//     // Create an education string with the form data
-//     const newEducation = `${school}`; // You can add other education fields here...
-
-//     onSubmit(newEducation);
-//   };
-
-//   return (
-//     <div>
-//       <h3>Add Education</h3>
-//       {/* <Autocomplete
-//         freeSolo
-//         options={fetchSchoolNames}
-//         renderInput={(params) => (
-//           <TextField
-//             {...params}
-//             label="School Name"
-//             value={school}
-//             onChange={(e) => setSchool(e.target.value)}
-//           />
-//         )}
-//       /> */}
-//       <input type="text" value={school} onChange={(e) => setSchool(e.target.value)} />
-//       {/* Add other education fields here... */}
-//       <button onClick={handleSubmit}>Save</button>
-//       <button onClick={onCancel}>Cancel</button>
-//     </div>
-//   );
-// };
-
-// export default EducationForm;
-
-// import React, { useState } from 'react';
-// import styled from 'styled-components';
-
-// interface EducationFormProps {
-//   onSubmit: (education: EducationData) => void;
-//   onCancel: () => void;
-// }
-
-// interface EducationData {
-//   school: string;
-//   degree: string;
-//   fieldOfStudy: string;
-//   startYear: string;
-//   endYear: string;
-//   grade: string;
-//   description: string;
-// }
-
-// const ModalWrapper = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   position: fixed;
-//   top: 0;
-//   left: 0;
-//   width: 100%;
-//   height: 100%;
-//   background-color: rgba(0, 0, 0, 0.5);
-// `;
-
-// const ModalContent = styled.div`
-//   background-color: #fff;
-//   padding: 20px;
-//   border-radius: 8px;
-// `;
-
-// const InputField = styled.input`
-//   width: 100%;
-//   padding: 8px;
-//   margin-bottom: 16px;
-//   border: 1px solid #ccc;
-//   border-radius: 4px;
-// `;
-
-// const TextArea = styled.textarea`
-//   width: 100%;
-//   padding: 8px;
-//   margin-bottom: 16px;
-//   border: 1px solid #ccc;
-//   border-radius: 4px;
-//   resize: vertical;
-// `;
-
-// const Button = styled.button`
-//   padding: 8px 16px;
-//   background-color: #007bff;
-//   color: #fff;
-//   border: none;
-//   border-radius: 4px;
-//   cursor: pointer;
-// `;
-
-// const EducationForm: React.FC<EducationFormProps> = ({ onSubmit, onCancel }) => {
-//   const [school, setSchool] = useState<string>('');
-//   const [degree, setDegree] = useState<string>('');
-//   const [fieldOfStudy, setFieldOfStudy] = useState<string>('');
-//   const [startYear, setStartYear] = useState<string>('');
-//   const [endYear, setEndYear] = useState<string>('');
-//   const [grade, setGrade] = useState<string>('');
-//   const [description, setDescription] = useState<string>('');
-
-//   const handleSubmit = () => {
-//     // Validate the data if needed
-
-//     // Create an education object with the form data
-//     const newEducation: EducationData = {
-//       school,
-//       degree,
-//       fieldOfStudy,
-//       startYear,
-//       endYear,
-//       grade,
-//       description,
-//     };
-
-//     onSubmit(newEducation);
-//   };
-
-//   return (
-//     <ModalWrapper>
-//       <ModalContent>
-//         <h3>Add Education</h3>
-//         <InputField
-//           type="text"
-//           value={school}
-//           onChange={(e) => setSchool(e.target.value)}
-//           placeholder="School Name"
-//         />
-//         <InputField
-//           type="text"
-//           value={degree}
-//           onChange={(e) => setDegree(e.target.value)}
-//           placeholder="Degree"
-//         />
-//         <InputField
-//           type="text"
-//           value={fieldOfStudy}
-//           onChange={(e) => setFieldOfStudy(e.target.value)}
-//           placeholder="Field of Study"
-//         />
-//         <InputField
-//           type="text"
-//           value={startYear}
-//           onChange={(e) => setStartYear(e.target.value)}
-//           placeholder="Start Year"
-//         />
-//         <InputField
-//           type="text"
-//           value={endYear}
-//           onChange={(e) => setEndYear(e.target.value)}
-//           placeholder="End Year (or expected)"
-//         />
-//         <InputField
-//           type="text"
-//           value={grade}
-//           onChange={(e) => setGrade(e.target.value)}
-//           placeholder="Grade"
-//         />
-//         <TextArea
-//           value={description}
-//           onChange={(e) => setDescription(e.target.value)}
-//           placeholder="Description"
-//         ></TextArea>
-//         <div>
-//           <Button onClick={handleSubmit}>Save</Button>
-//           <Button onClick={onCancel} style={{ marginLeft: 8, backgroundColor: '#dc3545' }}>
-//             Cancel
-//           </Button>
-//         </div>
-//       </ModalContent>
-//     </ModalWrapper>
-//   );
-// };
-
-// export default EducationForm;
-
-
-import React, { useState } from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import styled from 'styled-components';
 import Modal from 'react-modal';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+// import DatePicker from 'react-datepicker';
+// import 'react-datepicker/dist/react-datepicker.css';
+import useSWR from 'swr';
 
 interface EducationFormProps {
-  isOpen: boolean;
-  onClose: () => void;
-  onSubmit: (education: EducationData) => void;
+	isOpen: boolean;
+	onClose: () => void;
+	onSubmit: (education: EducationData) => void;
 }
 
 interface EducationData {
-  school: string;
-  degree: string;
-  fieldOfStudy: string;
-  startYear: string;
-  endYear: string;
-  description: string;
+	school: string;
+	degree: string;
+	fieldOfStudy: string;
+	startMonth: string;
+	startYear: string;
+	endMonth: string;
+	endYear: string;
+	description: string;
 }
 
+const ModalHeader = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	min-width: 100%;
+	padding: 20px 0;
+`;
+
+const ModalTitle = styled.h3`
+	margin: 0;
+`;
+
+const CloseButton = styled.button`
+	background: none;
+	border: none;
+	cursor: pointer;
+	font-size: 18px;
+`;
+
 const ModalContent = styled.div`
-  padding: 20px;
+	padding: 0 20px 20px;
+	position: relative;
+`;
+
+const FormLabel = styled.label`
+	font-size: small;
+	margin-bottom: 8px;
 `;
 
 const InputField = styled.input`
-  width: 100%;
-  padding: 8px;
-  margin-bottom: 16px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
+width: 100%;
+padding: 8px;
+margin: 5px 0 10px;
+border: 1px solid #ccc;
+border-radius: 4px;
+`;
+
+const SelectField = styled.select`
+	width: 100%;
+	padding: 8px;
+	// margin-bottom: 16px;
+	border: 1px solid #ccc;
+	border-radius: 4px;
+  flex: 0.45;
 `;
 
 const DatePickerContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 16px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin-bottom: 16px;
 `;
 
 const DateLabel = styled.label`
-  font-weight: bold;
+	font-size: small;
 `;
 
 const DescriptionField = styled.textarea`
-  width: 100%;
-  padding: 8px;
-  margin-bottom: 16px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  resize: vertical;
+	width: 100%;
+	padding: 8px;
+	margin-bottom: 16px;
+	border: 1px solid #ccc;
+	border-radius: 4px;
+	resize: vertical;
+`;
+
+const ButtonContainer = styled.div`
+	display: flex;
+	justify-content: flex-end;
 `;
 
 const Button = styled.button`
-  padding: 8px 16px;
-  background-color: #007bff;
-  color: #fff;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
+	padding: 8px 16px;
+	background-color: #292929;
+	color: #f3f3f3;;
+	border: none;
+	border-radius: 4px;
+	cursor: pointer;
+	margin-left: 8px;
 `;
 
-const EducationForm: React.FC<EducationFormProps> = ({ isOpen, onClose, onSubmit }) => {
-  const [school, setSchool] = useState<string>('');
-  const [degree, setDegree] = useState<string>('');
-  const [fieldOfStudy, setFieldOfStudy] = useState<string>('');
-  const [startYear, setStartYear] = useState<Date | null>(null);
-  const [endYear, setEndYear] = useState<Date | null>(null);
-  const [description, setDescription] = useState<string>('');
+const SuggestionsDropdown = styled.div`
+	position: absolute;
+	background-color: #fff;
+	border: 1px solid #ccc;
+	border-radius: 4px;
+	box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+	max-height: 200px;
+	max-width: -webkit-fill-available;
+	overflow-y: auto;
+	z-index: 1;
+	width: 100%;
+	padding: 8px;
+`;
 
-  const handleSubmit = () => {
-    // Validate the data if needed
-    if (!school || !degree || !fieldOfStudy || !startYear || !endYear) {
-      alert('Please fill in all mandatory fields.');
-      return;
-    }
+const Option = styled.div`
+	padding: 8px 0;
+	cursor: pointer;
 
-    // Create an education object with the form data
-    const newEducation: EducationData = {
-      school,
-      degree,
-      fieldOfStudy,
-      startYear: startYear!.getFullYear().toString(),
-      endYear: endYear!.getFullYear().toString(),
-      description,
-    };
+	&:hover {
+		background-color: #f8f8f8;
+	}
+`;
 
-    onSubmit(newEducation);
-    onClose();
-  };
+const EducationForm: React.FC<EducationFormProps> = ({
+	isOpen,
+	onClose,
+	onSubmit,
+}) => {
+	const [school, setSchool] = useState<string>('');
+	const [degree, setDegree] = useState<string>('');
+	const [fieldOfStudy, setFieldOfStudy] = useState<string>('');
+	const [startMonth, setStartMonth] = useState<string>('');
+	const [startYear, setStartYear] = useState<string>('');
+	const [endMonth, setEndMonth] = useState<string>('');
+	const [endYear, setEndYear] = useState<string>('');
+	const [description, setDescription] = useState<string>('');
 
-  return (
-    <Modal
-      isOpen={isOpen}
-      onRequestClose={onClose}
-      style={{
-        content: {
-          width: '400px',
-          margin: 'auto',
-        },
-        overlay: {
-          backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        },
-      }}
-    >
-      <ModalContent>
-        <h3>Add Education</h3>
-        <InputField
-          type="text"
-          value={school}
-          onChange={(e) => setSchool(e.target.value)}
-          placeholder="School Name"
-        />
-        <InputField
-          type="text"
-          value={degree}
-          onChange={(e) => setDegree(e.target.value)}
-          placeholder="Degree"
-        />
-        <InputField
-          type="text"
-          value={fieldOfStudy}
-          onChange={(e) => setFieldOfStudy(e.target.value)}
-          placeholder="Field of Study"
-        />
-        <DatePickerContainer>
-          <DateLabel>Start Year:</DateLabel>
-          <DatePicker
-            selected={startYear}
-            onChange={(date) => setStartYear(date)}
-            dateFormat="yyyy"
-            showYearPicker
-            scrollableYearDropdown
-          />
-        </DatePickerContainer>
-        <DatePickerContainer>
-          <DateLabel>End Year (or expected):</DateLabel>
-          <DatePicker
-            selected={endYear}
-            onChange={(date) => setEndYear(date)}
-            dateFormat="yyyy"
-            showYearPicker
-            scrollableYearDropdown
-          />
-        </DatePickerContainer>
-        <DescriptionField
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          placeholder="Description"
-        />
-        <div>
-          <Button onClick={handleSubmit}>Save</Button>
-          <Button onClick={onClose} style={{ marginLeft: 8, backgroundColor: '#dc3545' }}>
-            Cancel
-          </Button>
-        </div>
-      </ModalContent>
-    </Modal>
-  );
+	const currentYear = new Date().getFullYear();
+	const years = Array.from({ length: currentYear - 1950 + 1 }, (_, index) =>
+		(currentYear - index).toString()
+	);
+
+	// Function to fetch school suggestions using SWR
+	const fetcher = async (url: string) => {
+		const response = await fetch(url);
+		return response.json();
+	};
+
+	const { data: suggestions } = useSWR<{ name: string }[]>(
+		school
+			? `http://universities.hipolabs.com/search?name=${school}`
+			: null,
+		fetcher
+	);
+
+	const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+		setSchool(event.target.value);
+	};
+
+	const handleSuggestionSelect = (selectedSchool: string) => {
+		setSchool(selectedSchool);
+	};
+
+	const handleSubmit = () => {
+		// Validate the data if needed
+		if (
+			!school ||
+			!degree ||
+			!fieldOfStudy ||
+			!startMonth ||
+			!startYear ||
+			!endMonth ||
+			!endYear
+		) {
+			alert('Please fill in all mandatory fields.');
+			return;
+		}
+
+		// Create an education object with the form data
+		const newEducation: EducationData = {
+			school,
+			degree,
+			fieldOfStudy,
+			startMonth,
+			startYear,
+			endMonth,
+			endYear,
+			description,
+		};
+
+		onSubmit(newEducation);
+
+		// Clear the form fields after submitting
+		setSchool('');
+		setDegree('');
+		setFieldOfStudy('');
+		setStartMonth('');
+		setStartYear('');
+		setEndMonth('');
+		setEndYear('');
+		setDescription('');
+
+		onClose();
+	};
+
+	const handleCancel = () => {
+		// Clear the form fields on cancel
+		setSchool('');
+		setDegree('');
+		setFieldOfStudy('');
+		setStartMonth('');
+		setStartYear('');
+		setEndMonth('');
+		setEndYear('');
+		setDescription('');
+
+		onClose();
+	};
+
+	return (
+		<Modal
+			isOpen={isOpen}
+			onRequestClose={handleCancel}
+			style={{
+				content: {
+					width: '600px',
+					height: 'fit-content',
+					margin: 'auto',
+				},
+				overlay: {
+					backgroundColor: 'rgba(0, 0, 0, 0.5)',
+				},
+			}}
+		>
+			<ModalContent>
+				<ModalHeader>
+					<ModalTitle>Add new Education</ModalTitle>
+					<CloseButton onClick={handleCancel}>X</CloseButton>
+				</ModalHeader>
+				<FormLabel>School Name</FormLabel>
+				<InputField
+					type='text'
+					value={school}
+					onChange={handleSearchChange}
+					placeholder='Search for School Name'
+				/>
+				{/* Suggestions dropdown */}
+				{suggestions && suggestions.length > 0 && (
+					<SuggestionsDropdown>
+						{suggestions.map((suggestion, index) => (
+							<Option
+								key={index}
+								onClick={() =>
+									handleSuggestionSelect(suggestion.name)
+								}
+							>
+								{suggestion.name}
+							</Option>
+						))}
+					</SuggestionsDropdown>
+				)}
+				<FormLabel>Degree</FormLabel>
+				<InputField
+					type='text'
+					value={degree}
+					onChange={(e) => setDegree(e.target.value)}
+					placeholder='Degree'
+				/>
+				<FormLabel>Field of Study</FormLabel>
+				<InputField
+					type='text'
+					value={fieldOfStudy}
+					onChange={(e) => setFieldOfStudy(e.target.value)}
+					placeholder='Field of Study'
+				/>
+        <DateLabel>Start Date:</DateLabel>
+				<DatePickerContainer>
+					
+					<SelectField
+						value={startMonth}
+						onChange={(e) => setStartMonth(e.target.value)}
+					>
+						<option value=''>Select Month</option>
+						<option value='January'>January</option>
+						<option value='February'>February</option>
+						{/* Add more months here */}
+					</SelectField>
+					<SelectField
+						value={startYear}
+						onChange={(e) => setStartYear(e.target.value)}
+					>
+						<option value=''>Select Year</option>
+						{years.map((year) => (
+							<option key={year} value={year}>
+								{year}
+							</option>
+						))}
+					</SelectField>
+				</DatePickerContainer>
+        <DateLabel>End Date (or expected):</DateLabel>
+				<DatePickerContainer>
+					
+					<SelectField
+						value={endMonth}
+						onChange={(e) => setEndMonth(e.target.value)}
+					>
+						<option value=''>Select Month</option>
+						<option value='January'>January</option>
+						<option value='February'>February</option>
+						{/* Add more months here */}
+					</SelectField>
+					<SelectField
+						value={endYear}
+						onChange={(e) => setEndYear(e.target.value)}
+					>
+						<option value=''>Select Year</option>
+						{years.map((year) => (
+							<option key={year} value={year}>
+								{year}
+							</option>
+						))}
+					</SelectField>
+				</DatePickerContainer>
+				<FormLabel>Description</FormLabel>
+				<DescriptionField
+					value={description}
+					onChange={(e) => setDescription(e.target.value)}
+					placeholder='Description'
+				/>
+				<ButtonContainer>
+					<Button onClick={handleSubmit}>Save</Button>
+					<Button
+						onClick={handleCancel}
+						style={{ backgroundColor: 'transparent', color: '#292929', border: '1px solid #292929'  }}
+					>
+						Cancel
+					</Button>
+				</ButtonContainer>
+			</ModalContent>
+		</Modal>
+	);
 };
 
 export default EducationForm;
-
-
